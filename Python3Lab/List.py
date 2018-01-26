81e56b6748da8f8dd818af943f5d98ef384f109e
YeoGiyong <yeogiyong@gmail.com>
2018-01-26 18:01:36 +0900

    2018-01-26

--- Python3Lab/List.py
+++ Python3Lab/List.py
@@ -224,6 +224,8 @@ print(z)
 a, b, c = 1, 2, 3       #변수 초기화에 패킹, 언패킹 사용
 
 #연습문제 풀이
+
+#6 리스트 조작 함수에 대한 내용
 x = [1,2,3,4,5,6,7,8,9]
 
 print(x)
@@ -256,7 +258,49 @@ print(x.count(4))           #요소 수
 
 print(x.index(5))          #요소의 위치값 출력
 
+
+#7 다음을 실행하면 저장된 값은 몇개 인가?
 z = {1,1,1,2,2,3,3,3}
 print(z)                    #요소는 모두 3개
-z.add(1)                    #으이미 없는 코드
+z.add(1)                    #의미 없는 코드
 print(z)                    #어쨌든 3개
+
+
+#43 다음 코드의 결과는 무엇이 출력되는가?
+def myRange(start,end,hop =1):
+    retVal = start
+
+    while retVal <= end:
+        yield retVal
+        retVal +=hop
+
+hap = 0
+for i in myRange(1,5,2):       #종료값이 포함된 range 함수 작성
+                                #결국, 리스트 형태의 값이 출력
+#for i in range(1,5,2):     #i : 1,3
+#for i in [1,3,5] :      #i : 1,3,5
+    hap +=i
+print(hap)
+
+def myRange2(start,end,hop =1):
+    retVal = start
+
+    while retVal<=end:
+        #return retVal ??   #중간 계산결과를 출력 또는 처리
+        yield retVal        #실행중에 계산된 값은
+                            #generator 타입에 저장해 둠
+        retVal +=hop
+
+myRange2(1,5,2)
+a= myRange2(1,5,2)          #yield로 넘긴 데이터는 순환형식의
+                            #generator 타입 생성
+print(a)
+print(next(a))              #generator 타입에 저장된 값은
+                            #iterator 형식으로 다룰 수 있음
+                            #iterator는 리스트에 저장된 객체를
+                            #순환하며 하나씩 거내 사용하는 자료구조
+print(next(a))              
+print(next(a))
+
+for i in a:                 #generator 타입에 저장된 값은
+    print(i)                #for문으로도 출력 가능
